8cc19230ec42ae2a85f3d6b139463483f9c75f95
showkeyjar <710633139@qq.com>
2019-04-06 18:55:08 +0800

    modify print for python3

--- algorithm/ranking/CUNE_BPR.py
+++ algorithm/ranking/CUNE_BPR.py
@@ -122,8 +122,8 @@ from structure.symmetricMatrix import SymmetricMatrix
 #                 self.code[root.id] = prefix
 #
 #             # if root.id:
-#             #     print 'level', hierarchy
-#             #     print root.id,prefix,root.weight
+#             #     print('level', hierarchy
+#             #     print(root.id,prefix,root.weight
 #
 #             self.coding(root.left,prefix+'0',hierarchy+1)
 #             self.coding(root.right,prefix+'1',hierarchy+1)
@@ -148,16 +148,16 @@ class CUNE_BPR(IterativeRecommender):
 
     def printAlgorConfig(self):
         super(CUNE_BPR, self).printAlgorConfig()
-        print 'Specified Arguments of', self.config['recommender'] + ':'
-        print 'Walks count per user', self.walkCount
-        print 'Length of each walk', self.walkLength
-        print 'Dimension of user embedding', self.walkDim
-        print '='*80
+        print('Specified Arguments of', self.config['recommender'] + ':')
+        print('Walks count per user', self.walkCount)
+        print('Length of each walk', self.walkLength)
+        print('Dimension of user embedding', self.walkDim)
+        print('='*80)
 
     def buildModel(self):
-        print 'Kind Note: This method will probably take much time.'
+        print('Kind Note: This method will probably take much time.')
         #build C-U-NET
-        print 'Building collaborative user network...'
+        print('Building collaborative user network...')
         #filter isolated nodes
         self.itemNet = {}
         for item in self.data.trainSet_i:
@@ -175,7 +175,7 @@ class CUNE_BPR(IterativeRecommender):
         for user1 in self.filteredRatings:
             s1 = set(self.filteredRatings[user1])
             for user2 in self.filteredRatings:
-                if user1 <> user2:
+                if user1 != user2:
                     s2 = set(self.filteredRatings[user2])
                     weight = len(s1.intersection(s2))
                     if weight > 0:
@@ -184,7 +184,7 @@ class CUNE_BPR(IterativeRecommender):
 
         #build Huffman Tree First
         #get weight
-        # print 'Building Huffman tree...'
+        # print('Building Huffman tree...'
         # #To accelerate the method, the weight is estimated roughly
         # nodes = {}
         # for user in self.CUNet:
@@ -201,11 +201,11 @@ class CUNE_BPR(IterativeRecommender):
         #         pass
         # self.HTree = HuffmanTree(vecLength=self.walkDim)
         # self.HTree.buildTree(nodeList)
-        # print 'Coding for all users...'
+        # print('Coding for all users...'
         # self.HTree.coding(self.HTree.root,'',0)
 
 
-        print 'Generating random deep walks...'
+        print('Generating random deep walks...')
         self.walks = []
         self.visited = defaultdict(dict)
         for user in self.CUNet:
@@ -225,11 +225,11 @@ class CUNE_BPR(IterativeRecommender):
                     self.visited[user][nextNode] = 1
                     lastNode = nextNode
                 self.walks.append(path)
-                #print path
+                #print(path
         shuffle(self.walks)
 
         #Training get top-k friends
-        print 'Generating user embedding...'
+        print('Generating user embedding...')
         # iteration = 1
         # while iteration <= self.epoch:
         #     loss = 0
@@ -253,12 +253,12 @@ class CUNE_BPR(IterativeRecommender):
         #                         self.HTree.vector[prefix] += self.lRate*(1-sigmoid(w.dot(x)))*x
         #                         self.HTree.vector[centerCode] += self.lRate*(1-sigmoid(w.dot(x)))*w
         #                         loss += -log(sigmoid(w.dot(x)),2)
-        #     print 'iteration:', iteration, 'loss:', loss
+        #     print('iteration:', iteration, 'loss:', loss
         #     iteration+=1
         model = w2v.Word2Vec(self.walks, size=self.walkDim, window=5, min_count=0, iter=3)
-        print 'User embedding generated.'
+        print('User embedding generated.')
 
-        print 'Constructing similarity matrix...'
+        print('Constructing similarity matrix...')
         self.W = np.random.rand(self.data.trainingSize()[0], self.walkDim) / 10
         self.topKSim = {}
         i = 0
@@ -269,19 +269,19 @@ class CUNE_BPR(IterativeRecommender):
             u1 = self.data.user[user1]
             self.W[u1] = model.wv[user1]
             for user2 in self.CUNet:
-                if user1 <> user2:
+                if user1 != user2:
                     u2 = self.data.user[user2]
                     self.W[u2] = model.wv[user2]
                     sims.append((user2,cosine(self.W[u1],self.W[u2])))
             self.topKSim[user1] = sorted(sims, key=lambda d: d[1], reverse=True)[:self.topK]
             i += 1
             if i % 200 == 0:
-                print 'progress:', i, '/', len(self.CUNet)
-        print 'Similarity matrix finished.'
-        #print self.topKSim
+                print('progress:', i, '/', len(self.CUNet))
+        print('Similarity matrix finished.')
+        #print(self.topKSim
 
         #prepare Pu set, IPu set, and Nu set
-        print 'Preparing item sets...'
+        print('Preparing item sets...')
         self.PositiveSet = defaultdict(dict)
         self.IPositiveSet = defaultdict(dict)
         #self.NegativeSet = defaultdict(list)
@@ -298,7 +298,7 @@ class CUNE_BPR(IterativeRecommender):
                         self.IPositiveSet[user][item]=1
 
 
-        print 'Training...'
+        print('Training...')
         iteration = 0
         while iteration < self.maxIter:
             self.loss = 0
